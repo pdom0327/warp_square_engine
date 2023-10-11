a564da880746e80b017dbee6404bab5d6db72c17
Pdom <pdom0327@gmail.com>
2023-10-12 08:46:53 +0900

    feat: java

--- java/src/lib.rs
+++ java/src/lib.rs
@@ -1,14 +1,15 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#![allow(
+    clippy::enum_variant_names,
+    clippy::unused_unit,
+    clippy::let_and_return,
+    clippy::not_unsafe_ptr_arg_deref,
+    clippy::cast_lossless,
+    clippy::blacklisted_name,
+    clippy::too_many_arguments,
+    clippy::trivially_copy_pass_by_ref,
+    clippy::let_unit_value,
+    clippy::clone_on_copy
+)]
+mod jni_c_header;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+include!(concat!(env!("OUT_DIR"), "/java_glue.rs"));
