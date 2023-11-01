ef8504a80510e2f453e119a15f99f501dac50415
Pdom <pdom0327@gmail.com>
2023-11-02 04:40:01 +0900

    feat: square

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+mod square;
+
 pub fn get_hello(nickname: String) -> String {
     format!("Hello, {}!", nickname)
 }
---
+++ src/square.rs
@@ -0,0 +1,21 @@
+#[derive(Copy, Clone)]
+pub struct Square(u8);
+
+impl Square {
+    pub const A0: Square = Square(0);
+    pub const A1: Square = Square(1);
+    pub const A2: Square = Square(2);
+    pub const A3: Square = Square(3);
+    pub const A4: Square = Square(4);
+    pub const A5: Square = Square(5);
+    pub const A6: Square = Square(6);
+    pub const A7: Square = Square(7);
+    pub const A8: Square = Square(8);
+    pub const A9: Square = Square(9);
+    pub const AA: Square = Square(10);
+    pub const AB: Square = Square(11);
+    pub const AC: Square = Square(12);
+    pub const AD: Square = Square(13);
+    pub const AE: Square = Square(14);
+    pub const AF: Square = Square(15);
+}
